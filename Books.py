35341be3910d0f251aa9f52b76a19083c3f63657
JNarimatsu <julianadiquental@gmail.com>
2025-05-09 10:07:20 -0300

    Primeiro Commit do Projeto

---
+++ Books.py
@@ -0,0 +1,29 @@
+#print('Hello, world!')
+import streamlit as st #Pacote para visualizar o projeto
+import pandas as pd
+import  plotly.express as px
+
+
+st.set_page_config(layout = "wide") #Configurando para que a configuração d tela cheia persista
+
+st.title('Books')
+#Importando as tabelas com reviews e livros coletada do kaggle
+df_reviews = pd.read_csv(r'C:\Users\julia\Documents\Asimov\Python - Zero ao primeiro projeto\customer reviews.csv')
+df_top100_books = pd.read_csv(r'C:\Users\julia\Documents\Asimov\Python - Zero ao primeiro projeto\Top-100 Trending Books.csv')
+
+#Configuração do filtro de preços utilizando o componente slider do streamlit
+price_max = df_top100_books['book price'].max()
+price_min = df_top100_books['book price'].min()
+max_price = st.sidebar.slider('Price Range', price_min, price_max, price_max)
+df_books = df_top100_books[df_top100_books['book price']<= max_price]
+df_books
+
+#Colocando Gráficos 
+#filtrando livros por ano de publicação
+col1, col2 = st.columns(2) #Colocando os gráficos lado a lado 
+fig = px.bar(df_books['year of publication'].value_counts()) 
+col1.plotly_chart(fig)
+#Filtrando por preços
+fig_02 = px.histogram(df_books['book price'])
+col2.plotly_chart(fig_02)
+
---
+++ pages/Books_reviews.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout = "wide") #Configurando para que a configuração d tela cheia persista
+
+#Importando as tabelas com reviews e livros coletada do kaggle
+df_reviews = pd.read_csv(r'C:\Users\julia\Documents\Asimov\Python - Zero ao primeiro projeto\customer reviews.csv')
+df_top100_books = pd.read_csv(r'C:\Users\julia\Documents\Asimov\Python - Zero ao primeiro projeto\Top-100 Trending Books.csv')
+
+#Configurando o select box com os livros
+books = df_top100_books['book title'].unique()
+books = st.sidebar.selectbox('Books', books)
+df_book = df_top100_books[df_top100_books['book title'] == books]
+df_reviews_f = df_reviews[df_reviews['book name']==books]
+#Criando variáveis para organizar a pagina
+book_title = df_book['book title'].iloc[0]
+book_genre = df_book[ 'genre'].iloc[0]
+book_price = f"${df_book[ 'book price'].iloc[0]}"
+book_rating = df_book[ 'rating'].iloc[0]
+book_year = df_book[ 'year of publication'].iloc[0]
+#Configurando Layout da pagina
+st.title(book_title)
+st.subheader(book_genre)
+col1, col2, col3 = st.columns(3)
+col1.metric('price', book_price)
+col2.metric('Rating', book_rating)
+col3.metric('Year of Publication', book_year)
+#Configurando os reviews
+st.divider()
+for row in df_reviews_f.values:
+    message = st.chat_message(f"{row[4]}")
+    message.write(f"**{row[2]}**")
+    message.write(row[5])
